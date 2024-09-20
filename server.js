c70c5df8a6dd1426ee0f6207a66cd93e4271ca11
Sanskar Patel <sanskar.patel@qualcentric.net>
2024-09-21 00:48:07 +0530

    eff
    efewfew

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(cors({
   origin:"*"
 }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");  // Or 'http://localhost:3000'
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 
 const auth=require("./routes/auth")
